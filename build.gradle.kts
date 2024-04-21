2a975661636b37563d5ee07509d24b1d139fbf28
ObserverOfTime <chronobserver@disroot.org>
2024-04-21 13:14:41 +0300

    chore: release 0.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ allprojects {
 
 subprojects {
     group = "io.github.observeroftime.kbigint"
-    version = "0.1.2"
+    version = "0.2.0"
 
     if (System.getenv("CI") != null) {
         tasks.withType(AbstractTestTask::class) {
